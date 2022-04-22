2fee5397371af12d96b3c7686ce1c7395959b441
jipdesign-jp <jipdesigninfo@gmail.com>
2022-04-22 15:42:28 +0900

    added gitignore

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ df = {row.letter: row.code for index,row in phonetic.iterrows()}
 answer = input("Type a word you want. : ").upper()
 answer_letter = [letter for letter in answer if answer.isalpha()]
 answer_phonetic = {print(f"{letter} for {df[letter]}") for letter in answer_letter}
+
