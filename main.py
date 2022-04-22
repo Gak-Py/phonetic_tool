70c50db4a8976372bdc4bfe3a795999102da3fba
Gak_Py <jipdesigninfo@gmail.com>
2022-04-22 15:11:48 +0900

    firstt commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas
+phonetic = pandas.read_csv("phonetic_alphabet.csv")
+df = {row.letter: row.code for index,row in phonetic.iterrows()}
+
+answer = input("Type a word you want. : ").upper()
+answer_letter = [letter for letter in answer if answer.isalpha()]
+answer_phonetic = {print(f"{letter} for {df[letter]}") for letter in answer_letter}
